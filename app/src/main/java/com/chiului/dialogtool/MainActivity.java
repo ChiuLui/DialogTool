9a124766e618fab2c165b6a806754066d9747dfa
ChiuLui <906277645@qq.com>
2020-07-04 10:21:34 +0800

    创建 support 分支。

--- app/src/main/java/com/chiului/dialogtool/MainActivity.java
+++ app/src/main/java/com/chiului/dialogtool/MainActivity.java
@@ -1,8 +1,8 @@
 package com.chiului.dialogtool;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
